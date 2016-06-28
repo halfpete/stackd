e63ee76104d4aebff659b221fa05d07e0e33e052
Peter Lee <plee1@plee1-m01.local>
2016-06-28 13:54:17 -0700

    gitignore .pyc

--- stackd/overflow/models.py
+++ stackd/overflow/models.py
@@ -3,29 +3,33 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 
+
 # Create your models here.
 
 
-class Question (models.Model):
-	author = models.EmailField
-	question_title = models.TextField(max_length = 200)
-	question_detail = models.TextField
-	tags = models.CharField(max_length = 200)
-	pub_date = models.DateTimeField('date published')
-	status = models.CharField(max_length = 20)
-	upvotes = models.PositiveIntegerField (default = 0)
-	downvotes = models.PositiveIntegerField(default = 0)
-	netvotes = models.IntegerField(default = 0)
-	def __str__(self):
-		return self.question_title
+class Question(models.Model):
+    author = models.EmailField
+    question_title = models.TextField(max_length=200)
+    question_detail = models.TextField
+    tags = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    status = models.CharField(max_length=20)
+    upvotes = models.PositiveIntegerField(default=0)
+    downvotes = models.PositiveIntegerField(default=0)
+    netvotes = models.IntegerField(default=0)
 
-class Comment (models.Model):
-	question = models.ForeignKey(Question, on_delete = models.CASCADE)
-	comment_text = models.CharField(max_length = 200)
-	author = models.EmailField
-	upvotes = models.IntegerField(default = 0)
-	downvotes = models.IntegerField(default = 0)
-	netvotes = models.IntegerField(default = 0)
-	pub_date = models.DateTimeField('date published')
-	def __str__(self):
-		return self.comment_text
+    def __str__(self):
+        return self.question_title
+
+
+class Comment(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+    author = models.EmailField
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    netvotes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.comment_text
--- stackd/overflow/views.py
+++ stackd/overflow/views.py
@@ -7,18 +7,22 @@ from django.utils import timezone
 from django.template import loader, RequestContext
 from django.db import connection
 
+
 def index(request):
-	question_list = Question.objects.order_by('-pub_date')
-	template = loader.get_template('overflow/index.html')
-	context = {
-	'question_list': question_list,
-	}
-	return HttpResponse(template.render(context, request))
+    question_list = Question.objects.order_by('-pub_date')
+    template = loader.get_template('overflow/index.html')
+    context = {
+        'question_list': question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def add_new_question_to_database(request, title, detail, tags, email):
-	q = Question(question_title = title, question_detail = detail, tags = tags, author = email, pub_date = timezone.now(), status = 'unsolved')
-	q.save()
+    q = Question(question_title=title, question_detail=detail, tags=tags, author=email, pub_date=timezone.now(),
+                 status='unsolved')
+    q.save()
+
 
 def add_comment_to_question(request, question, text, email):
-	c = Comment(question = question, comment_text = text, author = email, pub_date = timezone.now())
-	c.save()
+    c = Comment(question=question, comment_text=text, author=email, pub_date=timezone.now())
+    c.save()
