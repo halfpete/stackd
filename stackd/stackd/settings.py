0b1fe4458958b4a9f862c869f72a7eb092e9abf1
Peter Lee <plee1@plee1-m01.local>
2016-06-28 11:25:33 -0700

    postgres settings setup

--- stackd/stackd/settings.py
+++ stackd/stackd/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'stackd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'stackd',
+        'USER': 'stackduser',
+        'PASSWORD': 'pandora',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
