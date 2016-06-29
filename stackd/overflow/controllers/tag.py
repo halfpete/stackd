2287ba70379f1da9b06d54d25a82ab8231acbc15
Peter Lee <plee1@plee1-m01.local>
2016-06-29 12:57:44 -0700

    getting rid of django programming error

--- stackd/overflow/controllers/tag.py
+++ stackd/overflow/controllers/tag.py
@@ -1,13 +1,20 @@
-from overflow.models import Tag
+from overflow.models import Tag, Question
 
 
-def get_name(request, primary_key):
-    t = Tag.objects.get(primary_key)
+def get_name(request, question):
+    t = Tag.objects.get(question)
     return t.name
 
 
-def set_name(request, primary_key, new_name):
-    t = Tag.objects.get(primary_key)
+def set_name(request, question, new_name):
+    t = Tag.objects.get(question)
     t.name = new_name
 
-# def add_tags
+
+def add_tags(request, question, *tag_names):
+    q = Question.objects.get(question.id)
+    for tag_name in tag_names:
+        q.tags.append(tag_name + ",")
+        t = Tag(question=question, name=tag_name)
+        t.save()
+
